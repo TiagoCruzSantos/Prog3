e1abec337980734014d5422cdf0b5d2dfa339b6b
Tiago Cruz <tiago.unity.cruz@gmail.com>
2019-09-24 10:37:38 -0300

    gitp triangulo

--- lista4/main.cpp
+++ lista4/main.cpp
@@ -13,15 +13,19 @@ int main(){
     Circulo *circ = new Circulo(3);
     circ->imprimirDados();
     cout << endl;
+
     Quadrado *quad = new Quadrado(5);
     quad->imprimirDados();
     cout << endl;
+
     Triangulo *tri = new Triangulo(5, 5);
     tri->imprimirDados();
     cout << endl;
+
     Esfera *esf = new Esfera(3);
     esf->imprimirDados();
     cout << endl;
+    
     delete circ;
     delete quad;
     delete tri;
